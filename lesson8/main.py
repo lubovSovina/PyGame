ec843c90ec5af8e4b72e6babc5fb72799e2f56cf
lubovSovina <sovinaliubov@yandex.ru>
2022-12-24 11:12:08 +0300

    Add lesson with firework

---
+++ lesson8/main.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import random
+import pygame
+
+GRAVITY = 0.1
+pygame.init()
+pygame.display.set_caption('Салют')
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+screen_rect = (0, 0, width, height)
+all_sprites = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('..\data', name)
+    if not os.path.isfile(fullname):
+        print(f'Файл с изображением {fullname} не найден')
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Particle(pygame.sprite.Sprite):
+    fire = [load_image('star.png')]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super(Particle, self).__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.gravity = GRAVITY
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    particle_count = 20
+    numbers = range(-5, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers))
+
+
+if __name__ == '__main__':
+    fps = 50
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                create_particles(pygame.mouse.get_pos())
+        all_sprites.update()
+        screen.fill(pygame.Color('#9D81BA'))
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
+
+
+
+
+
+
+
+
+
+
